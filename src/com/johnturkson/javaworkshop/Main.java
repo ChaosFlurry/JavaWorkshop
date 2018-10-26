cb832a562c488a7687c5528a79fb41c09b9c9cb1
ChaosFlurry <ChaosFlurry@gmail.com>
2018-10-26 03:27:18 -0700

    Added documentation to main() showing what it does

--- src/com/johnturkson/javaworkshop/Main.java
+++ src/com/johnturkson/javaworkshop/Main.java
@@ -3,7 +3,7 @@ package com.johnturkson.javaworkshop;
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World"); // prints "Hello World"
     }
     
 }
